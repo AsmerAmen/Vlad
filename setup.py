11f0e92c6e401e0813da47df946473c4bae1624a
AsmerAmen <asmer.amenar@gmail.com>
2018-07-05 20:59:56 +0200

    "V 1.0"
    Signed-off-by: AsmerAmen <asmer.amenar@gmail.com>

---
+++ setup.py
@@ -0,0 +1,38 @@
+# from distutils.core import setup
+# import py2exe
+
+# setup(console=['cVlad.py'])
+
+from cx_Freeze import setup, Executable
+import os
+os.environ['TCL_LIBRARY'] = "C:\\Users\\win8\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6"
+os.environ['TK_LIBRARY'] = "C:\\Users\\win8\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6"
+
+
+include_files = ['microphone.png',
+			'mic.ico',
+			'chime1.mp3',
+			'chime2.mp3',
+ 			os.path.join("C:\\Users\\win8\\AppData\\Local\\Programs\\Python\\Python36-32", 'DLLs', 'tk86t.dll'),
+            os.path.join("C:\\Users\\win8\\AppData\\Local\\Programs\\Python\\Python36-32", 'DLLs', 'tcl86t.dll'),
+            ]
+
+
+excludes = ['']
+packages = ["pygame", "speech_recognition", "webbrowser", "tkinter"]
+
+setup(name = 'cVlad',
+	version = '0.1',
+	description = 'VA',
+	options = {
+	"build_exe": {
+	    'packages': packages,
+	    'include_files': include_files,
+	    'excludes': excludes,
+	    'optimize' : 2,
+	    'include_msvcr': True,
+		}
+	},
+	executables = [Executable("cVlad.py")]
+
+	)
---
+++ wakeup.py
@@ -0,0 +1,93 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import time
+
+
+# Text To Speech
+
+eng = pyttsx3.init()
+voices = eng.getProperty('voices')
+eng.setProperty('voice', voices[2].id)
+
+# High priorty commands
+
+WAKE_UP = ["on", "wake up"]
+TERMINATE = ["shut down", "terminate", "quit"]
+CANCEL = ["cancel", "back"]
+
+
+def wake_up():
+    while True:
+
+        r = sr.Recognizer()
+
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source, timeout= None)
+
+            try:
+                print(r.recognize_google(audio))
+                if r.recognize_google(audio) in WAKE_UP:
+                    print("Up")
+                    eng.say('Up')
+                    eng.runAndWait()
+
+                    r2 = sr.Recognizer()
+
+                    with sr.Microphone() as source2:
+                        r.adjust_for_ambient_noise(source2)
+                        audio2 = r2.listen(source2, timeout= 5)
+
+                    try:
+                        cmd = r2.recognize_google(audio2)
+
+                        print(cmd)
+                        eng.say(cmd)
+                        eng.runAndWait()
+
+                        if cmd is 'play music':
+                            webbrowser.open('https://play.anghami.com/mymusic/')
+
+                        elif cmd.endswith('anghami'):
+                            webbrowser.open('https://play.anghami.com/search/'+ cmd[5:-10])
+
+                        elif cmd.startswith('open'):
+                            webbrowser.open('https://www.%s.com/' % cmd[5:])
+
+                        else:
+                            pass
+
+                    except sr.UnknownValueError:
+                        eng.say("I'm sorry, I couldn't understand you")
+                        eng.runAndWait()
+                        print("I'm sorry, I couldn't understand you")
+
+                    except sr.RequestError:
+                        eng.say("I'm sorry, I couldn't reach google")
+                        eng.runAndWait()
+                        print("I'm sorry, I couldn't reach google")
+
+
+                elif r.recognize_google(audio) in TERMINATE:
+                    eng.say("Shutting down")
+                    eng.runAndWait()
+                    print("Shutting down...")
+                    break
+
+                elif r.recognize_google(audio) in CANCEL:
+                    eng.say("Stand by mode")
+                    eng.runAndWait()
+                    print("Stand by mode...")
+                    pass
+
+            except sr.UnknownValueError:
+                print("Listening to the magic words: 'Wake up ' or 'Shut down'")
+
+            except sr.RequestError:
+                eng.say("I'm sorry, I couldn't reach google")
+                eng.runAndWait()
+                print("I'm sorry, I couldn't reach google")
+
+if __name__ == '__main__':
+    wake_up()
